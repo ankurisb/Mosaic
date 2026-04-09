a8e55dbe00f5d94858d256fbfcd2665a56d6fefa
nkursinhg <105057873+nkursinhg@users.noreply.github.com>
2026-04-09 12:45:09 +0100

    Delete app directory

--- app/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-import { getSession } from '@/lib/auth'
-import SettingsPage from '@/components/settings/SettingsPage'
-
-export default async function Settings() {
-  const session = await getSession()
-  if (!session) redirect('/login')
-  return <SettingsPage user={session} />
-}
